dc8c9265efd26eff973418efba354db3669df4f9
Vincent Xu <vincent.xu1@outlook.com>
2025-07-14 15:51:39 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+
+namespace Assignment_2._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a shape:");
+            Console.WriteLine("a) circle");
+            Console.WriteLine("b) square");
+            String? input = Console.ReadLine();
+
+            Shape? shape = null;
+            switch (input)
+            {
+                case "a":
+                    Console.WriteLine("Please provide radius:");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+                    shape = new Circle() { Radius = radius };
+                    break;
+                case "b":
+                    Console.WriteLine("Please provide side:");
+                    double side = Convert.ToDouble(Console.ReadLine());
+                    shape = new Square() { Side = side };
+                    break;
+            }
+            if (shape != null)
+                Console.WriteLine($"Area is: {shape.CalculateArea()}");
+        }
+    }
+    public abstract class Shape
+    {
+        public int ID { get; set; }
+        public String? Name { get; set; }
+        public Color Color { get; set; }
+        public abstract double CalculateArea();
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+        public override double CalculateArea()
+        {
+            return Side * Side;
+        }
+    }
+}
